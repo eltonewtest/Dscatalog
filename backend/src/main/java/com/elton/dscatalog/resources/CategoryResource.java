68de3fb780e9f231cb836bfa1377a1bc392cd889
eltonewtest <elton.castro.up@gmail.com>
2022-11-01 20:25:15 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/elton/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/elton/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,18 @@ public class CategoryResource {
 	
 	@Autowired
 	private CategoryService catService;
+	
 	@GetMapping
-	public ResponseEntity<List<CategoryDto>> finAll(){
+	public ResponseEntity<List<CategoryDto>> findAll(){
 		List<CategoryDto> list = catService.findAll();
 	
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDto> findById(@PathVariable Long id){
+		CategoryDto dto = catService.findById(id);
+	
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/elton/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/elton/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.elton.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.elton.dscatalog.dto.CategoryDto;
 import com.elton.dscatalog.entities.Category;
 import com.elton.dscatalog.repositories.CategoryRepository;
+import com.elton.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -24,4 +26,21 @@ public class CategoryService {
 		//Tranformar lista de Category em CategpryDto
 		return list.stream().map(x -> new CategoryDto(x) ).collect(Collectors.toList());
 	}
+	
+	
+	@Transactional(readOnly = true)
+	public CategoryDto findById(Long id) {
+		Optional<Category> obj =  catRepository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+		return new CategoryDto(entity);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
