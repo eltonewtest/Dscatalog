8f66b648785d40204a916efc82fa9dcebdbde1db
eltonewtest <elton.castro.up@gmail.com>
2022-11-03 21:52:48 -0300

    Created product entity, SQL script

--- backend/src/main/java/com/elton/dscatalog/entities/Category.java
+++ backend/src/main/java/com/elton/dscatalog/entities/Category.java
@@ -2,19 +2,21 @@ package com.elton.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_category")
+@Table(name = "tb_category")            
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -22,20 +24,28 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-
+	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
-	public Category() {}
-
+	public Category() {
+	}
+	
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
 	public Long getId() {
 		return id;
 	}
-
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -47,19 +57,10 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
-	}
-
-	
-	
 	
 	public Instant getCreatedAt() {
 		return createdAt;
 	}
-
 
 	public Instant getUpdatedAt() {
 		return updatedAt;
@@ -68,14 +69,25 @@ public class Category implements Serializable{
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
-	}
+	}	
 	
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
 	}
 	
+	public Set<Product> getProducts(){
+		return products;
+	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -85,10 +97,17 @@ public class Category implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Category other = (Category) obj;
-		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
-	
-	
+	public String toString() {
+		return name;
+	}
+
 	
 }
